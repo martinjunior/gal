6d6aea059551cd94a9c891acfcf2e62d336d2a33
Martin Duran <martinduran@Martins-iMac.local>
2015-03-30 22:55:53 -0500

    Added proper mail headers.

--- src/functions/send.php
+++ src/functions/send.php
@@ -5,10 +5,16 @@
 
     $htmlEmail = generate_html_contact_email($_POST);
 
+    // HTML contact email headers (required)
+    $headers = "Reply-To: ".$email."\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
     if (mail(
         'martinduranwebdesign@gmail.com',
         'Website Contact - Great American Landscaping',
-        $htmlEmail
+        $htmlEmail,
+        $headers
     )) {
         echo $htmlEmail;
     }
