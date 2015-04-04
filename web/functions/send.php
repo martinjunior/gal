ec07a3eadf2aaff36ed47621f57107cfcb67eb82
Martin Duran <martinduran@Martins-iMac.local>
2015-04-04 16:12:43 -0500

    Wrapped send() in reverse captcha check.

--- web/functions/send.php
+++ web/functions/send.php
@@ -5,18 +5,19 @@
 
     $htmlEmail = generate_html_contact_email($_POST);
 
-    // HTML contact email headers (required)
+    // HTML email headers (required)
     $headers = "Reply-To: ".$email."\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    if (mail(
-        'martinduranwebdesign@gmail.com',
-        'Website Contact - Great American Landscaping',
-        $htmlEmail,
-        $headers
-    )) {
-        echo $htmlEmail;
+    // Reverse captcha
+    if ($_POST['referral'] == '') {
+        mail(
+            'martinduranwebdesign@gmail.com',
+            'Website Contact - Great American Landscaping',
+            $htmlEmail,
+            $headers
+        );
     }
 
 ?>
